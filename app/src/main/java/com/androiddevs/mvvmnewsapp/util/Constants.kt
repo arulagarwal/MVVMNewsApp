a535fcbe32692ed9d9d5116e48f6755cc6ffac3e
Arul.Agarwal <arul.agarwal@viacom18.com>
2022-05-09 09:18:32 +0530

    Updated files

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.androiddevs.mvvmnewsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = ""//AddAPIKEY
+        const val API_KEY = "64d514cc270547c2a89bbd19227c9eeb"//AddAPIKEY
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
